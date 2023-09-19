1d4fb864f07e0a8727e612a580ffea004f241f01
NirajTharu <nirajc279@gmail.com>
2023-09-19 17:59:41 +0545

    inheritance
    Every type of inheritance...

--- Inheritance/4th_Hybrid_inheritance.cpp
+++ Inheritance/4th_Hybrid_inheritance.cpp
@@ -1,4 +1,46 @@
 //Hybrid Inheritance(combinaton of two types or more than 2 types of inheritance is Hybrid inheritance)
 #include<iostream>
 using namespace std;
-class
+class Living_being
+{   
+    public:
+    void display()
+    {   
+        cout<<"The organism which have life in them are called living organism."<<endl;
+    }
+};
+class bat:virtual public Living_being
+{
+    public:
+    void display0()
+    {
+        display();
+        cout<<"Bat is also known as mamal because it give direct birth to their baby."<<endl;
+    }
+};
+
+class Human:virtual public Living_being
+{
+    public:
+    void display1()
+    {   
+        display();
+        cout<<"Human Give direct birth to their baby and feed their own milk."<<endl;
+    }
+};
+class Mamal:public bat,public Human
+{
+    public:
+    void display2()
+    {   
+        display0();
+        display1();
+        cout<<"The animals which give direct birth to their baby and feed their own milk are called Mamal."<<endl;
+    }
+};
+int main()
+{
+    Mamal m;
+    m.display2();
+    return 0;
+}
--- Inheritance/5th_Hierarchical_inheritance.cpp
+++ Inheritance/5th_Hierarchical_inheritance.cpp
@@ -1,7 +1,38 @@
 //Hierarchical Inheritance
 #include<iostream>
 using namespace std;
-class animal
+class Sanskrit 
+{   
+    public:
+    void display()   
+    {
+        cout<<"The Sanskrit language is the mother of most of the language."<<endl;
+    } 
+};
+class English:public Sanskrit
+{   
+    public:
+    void display0()
+    {   
+        display();
+        cout<<"The English language is spoken all over the world."<<endl;
+    }
+};
+
+class Nepali:public Sanskrit
 {
-    
+    public:
+    void display1()
+    {   
+        display();
+        cout<<"The Nepali language is spoken all over the Nepal and is the National Language of Nepal."<<endl;
+    }
+};
+int main()
+{
+    English e;
+    Nepali n;
+    e.display0();
+    n.display1();
+    return 0;
 }
